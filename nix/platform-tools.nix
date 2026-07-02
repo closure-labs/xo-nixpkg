c8d98c0005a2ce47fcafa26f2699b06a5ee59b87
declarative-dale <52590374+declarative-dale@users.noreply.github.com>
2026-07-02 02:44:24 +0000

    xen-orchestra-ce: update to 6.6.0

--- nix/platform-tools.nix
+++ nix/platform-tools.nix
@@ -2,8 +2,8 @@
   x86_64-linux = {
     esbuild = null;
     turbo = {
-      version = "2.9.12";
-      tarball = "_turbo_linux_64___linux_64_2.9.12.tgz";
+      version = "2.9.17";
+      tarball = "_turbo_linux_64___linux_64_2.9.17.tgz";
       path = "turbo-linux-x64/bin/turbo";
     };
     rollup = null;
@@ -11,8 +11,8 @@
   aarch64-linux = {
     esbuild = null;
     turbo = {
-      version = "2.9.12";
-      tarball = "_turbo_linux_arm64___linux_arm64_2.9.12.tgz";
+      version = "2.9.17";
+      tarball = "_turbo_linux_arm64___linux_arm64_2.9.17.tgz";
       path = "turbo-linux-arm64/bin/turbo";
     };
     rollup = null;
